a2f10a6cb24ad2a925353fc3d83d279260faf934
Yashas Nadig <103478177+yashasnadigsyn@users.noreply.github.com>
2022-07-08 17:20:48 +0530

    Update mainixigo.py

--- mainixigo.py
+++ mainixigo.py
@@ -47,7 +47,7 @@ def find(update, context):
             options.log.level = "trace"
             options.add_argument("-remote-debugging-port=9224")
             options.add_argument("-headless")
-	    options.add_argument("-disable-gpu")
+            options.add_argument("-disable-gpu")
             options.add_argument("-no-sandbox")
             binary = FirefoxBinary(os.environ.get('FIREFOX_BIN'))
             driver = webdriver.Firefox(firefox_binary=binary, executable_path=os.environ.get('GECKODRIVER_PATH'), options=options)     
