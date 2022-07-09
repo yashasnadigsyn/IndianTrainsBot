58bd3bbf02d372da28b67b5c7cb359b4aa914dac
Yashas Nadig <103478177+yashasnadigsyn@users.noreply.github.com>
2022-07-09 07:12:15 +0530

    Update mainixigo.py

--- mainixigo.py
+++ mainixigo.py
@@ -13,7 +13,7 @@ from telegram import Bot
 import requests, pickle
 from bs4 import BeautifulSoup
 
-TOKEN = MYTOKEN
+TOKEN = 'MYTOKEN'
 
 def start(update, context):
     update.message.reply_text("Hi! I am a bot that will help you to find the cheapest train tickets.\n"
